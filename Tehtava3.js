7bcbbdff34bcefba0ab97eaa04ddac4c24db33a8
TenDRILLL <32621403+TenDRILLL@users.noreply.github.com>
2022-04-11 22:06:57 +0300

    Create Tehtava3.js

---
+++ Tehtava3.js
@@ -0,0 +1,9 @@
+// 3. Tee ohjelma joka tulostaa nimesi näytölle 10 kertaa.
+// Hyötykäytä alla annettua looppia.
+for(let i = 0; i < 21; i++){
+  console.log(i);
+}
+
+//let i = 0; - Alustetaan i arvolla 0.
+//i < 21; - Suorita kun 1 on alle 21
+//i++ - joka iteraation jälkeen, lisää i'hin 1
